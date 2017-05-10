8fd600709ced1801cc227529f40e3dab4d90b6c7
rickymare <rickypwnage@gmail.com>
2017-05-09 20:06:10 -0700

    installed nodebb

---
+++ web2/web2.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+# This script will install and configure NodeBB
+
+set -e
+set -u
+
+# Update the APT cache 
+apt update -y
+
+# Upgrade
+# apt upgrade -y
+
+# Install Nodejs, Nodejs-legacy, npm, and mongodb-server
+apt install nodejs nodejs-legacy npm mongodb-server -y
+
+# Clone the NodeBB package from Github
+git clone -b v1.5.x https://github.com/NodeBB/NodeBB.git nodebb
+
+# Install NodeBB
+npm install --production nodebb/
