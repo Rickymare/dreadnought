eb14e417266f129d9e34de2a0eefebd62cbbb3d9
rickymare <rickypwnage@gmail.com>
2017-05-11 12:51:15 -0700

    Corrected spelling mistake and made notes consistent

--- data/data.sh
+++ data/data.sh
@@ -6,13 +6,13 @@ set -u
 # Import GPG key to official MongoDB repository
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
 
-# Create a file list for MongoDB
+# Creating a file list for MongoDB
 echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
 
-# Update APT
+# Updating APT
 apt update -y
 
-# Install MongoDB package
+# Installing MongoDB package
 apt install -y mongodb-org
 
 # Creating a file to manage the MongoDB service.
@@ -27,10 +27,10 @@ ExecStart=/usr/bin/mongod --quiet --config /etc/mongod.conf
 [Install]
 WantedBy=multi-user.target" > /etc/systemd/system/mongodb.service
 
-# Staring the MongoDB server
+# Starting the MongoDB server
 systemctl start mongodb
 
-# This will automatically start MongoDB
+# Automatically start MongoDB when server starts
 systemctl enable mongodb
 
 echo "hey! I am at the end of the script"
